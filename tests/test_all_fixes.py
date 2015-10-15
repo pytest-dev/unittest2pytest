f0a3c80ad46974ef7d54a9457a5e5c2a4a2382df
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-10-15 22:29:46 +0200

    Add copyright header to all relevant files.

--- tests/test_all_fixes.py
+++ tests/test_all_fixes.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+This file is part of test-suite for unittest2pytest.
+"""
+#
+# Copyright 2015 by Hartmut Goebel <h.goebel@crazy-compilers.com>
+#
+# This program is part of unittest2pytest.
+#
+# unittest2pytest free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 from __future__ import unicode_literals
+
+__author__ = "Hartmut Goebel <h.goebel@crazy-compilers.com>"
+__copyright__ = "Copyright 2015 by Hartmut Goebel"
+__licence__ = "GNU General Public License version 3 or later (GPLv3+)"
+
 
 import pytest
 
--- unittest2pytest/utils.py
+++ unittest2pytest/utils.py
@@ -1,3 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Some utility functions for unittest2pytest.
+"""
+#
+# Copyright 2015 by Hartmut Goebel <h.goebel@crazy-compilers.com>
+#
+# This file is part of unittest2pytest.
+#
+# unittest2pytest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = "Hartmut Goebel <h.goebel@crazy-compilers.com>"
+__copyright__ = "Copyright 2015 by Hartmut Goebel"
+__licence__ = "GNU General Public License version 3 ot later (GPLv3+)"
+
+
 import inspect
 try:
     from inspect import Parameter
