fc89c13dacf8b4f663db9e2af40c4953332d5450
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-10-20 17:17:45 +0200

    Add fixtures for assertRaisesRegex/assertRaisesRegexp.

---
+++ tests/fixtures/self_assert/assertRaisesRegexp_out.py
@@ -0,0 +1,22 @@
+# required-method: assertRaisesRegexp
+
+class TestRaises(TestCase):
+    def test_simple(self):
+        with pytest.raises(RunTimeError) as excinfo:
+            someFunc()
+        assert re.search(pattern, excinfo.value)
+
+    def test_args(self):
+        with pytest.raises(RunTimeError) as excinfo:
+            someFunc(1,2,3)
+        assert re.search(pattern, excinfo.value)
+
+    def test_kwargs(self):
+        with pytest.raises(RunTimeError) as excinfo:
+            someFunc(foo=42, bar=43)
+        assert re.search(pattern, excinfo.value)
+
+    def test_args_kwargs(self):
+        with pytest.raises(RunTimeError) as excinfo:
+            someFunc(1,2,3, foo=42, bar=43)
+        assert re.search(pattern, excinfo.value)
