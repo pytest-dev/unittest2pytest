8a9b671549332194285b862d5aaddc6e55f2ee66
Adam Chainz <me@adamj.eu>
2016-05-25 10:56:53 +0100

    Make assertRaises / assertWarns context managers work
    Fixes #4. Note that since the semantics of what e.g. `assertRaises` captures is different to what `pytest.raises` captures, they can't easily be converted. It's recommended users manually fix up any cases after the fact - added a note to the README to indicate this.
    
    Also fixed the link to pytest in the README, it was pointing to PEP8.

--- tests/fixtures/self_assert/assertRaises_out.py
+++ tests/fixtures/self_assert/assertRaises_out.py
@@ -16,3 +16,12 @@ class TestRaises(TestCase):
     def test_args_kwargs(self):
         with pytest.raises(RunTimeError):
             someFunc(1,2,3, foo=42, bar=43)
+
+    def test_context_manager(self):
+        with pytest.raises(RunTimeError):
+            someFunc()
+
+    def test_context_manager_var(self):
+        with pytest.raises(RunTimeError) as ctx:
+            someFunc()
+        assert ctx.exception
