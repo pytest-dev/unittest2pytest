6c35b75c0f6bba606930de42b7eb04f86b12f6d6
Hartmut Goebel <h.goebel@crazy-compilers.com>
2017-08-16 18:40:18 +0200

    Fix syntax-errors in test-files for assertDictContainsSubset.
    Glad I added 2312deb4caba0883139fe57e510bffbddefa65dc just a few
    minites ago :-) Nevertheless I don't know why this did not show up in
    my working environment at first.

--- tests/fixtures/self_assert/assertDictContainsSubset_out.py
+++ tests/fixtures/self_assert/assertDictContainsSubset_out.py
@@ -5,16 +5,16 @@ class TestDictEqual(TestCase):
         assert dict(superset, **{'a: 1'}) == superset
 
     def test_simple_msg(self):
-        assert dict({'a: 1'}. **subset) == {'a: 1'}, "This is wrong!"
+        assert dict({'a: 1'}, **subset) == {'a: 1'}, "This is wrong!"
 
     def test_simple_msg2(self):
-        assert dict({'a: 1'}. **subset) == {'a: 1'}, "This is wrong!"
+        assert dict({'a: 1'}, **subset) == {'a: 1'}, "This is wrong!"
 
     def test_line_wrapping(self):
         assert dict({
                 'a': 1,
                 'b': 2,
-            }, {'b': 2}) == {
+            }, **{'b': 2}) == {
                 'a': 1,
                 'b': 2,
             }, \
