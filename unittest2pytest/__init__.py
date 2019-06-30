bb844e67999df10b080fd5eff9ccdd60c19c05fc
Hartmut Goebel <h.goebel@crazy-compilers.com>
2019-06-30 15:44:22 +0200

    Update copyright year.

--- unittest2pytest/__init__.py
+++ unittest2pytest/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2015-1017 by Hartmut Goebel <h.goebel@crazy-compilers.com>
+# Copyright 2015-2019 by Hartmut Goebel <h.goebel@crazy-compilers.com>
 #
 # This file is part of unittest2pytest.
 #
@@ -19,7 +19,7 @@
 #
 
 __author__ = "Hartmut Goebel <h.goebel@crazy-compilers.com>"
-__copyright__ = "Copyright 2015-1017 by Hartmut Goebel"
+__copyright__ = "Copyright 2015-2019 by Hartmut Goebel"
 __licence__ = "GNU General Public License version 3 or later (GPLv3+)"
 
 
--- unittest2pytest/utils.py
+++ unittest2pytest/utils.py
@@ -3,7 +3,7 @@
 Some utility functions for unittest2pytest.
 """
 #
-# Copyright 2015-1017 by Hartmut Goebel <h.goebel@crazy-compilers.com>
+# Copyright 2015-2019 by Hartmut Goebel <h.goebel@crazy-compilers.com>
 #
 # This file is part of unittest2pytest.
 #
@@ -22,7 +22,7 @@ Some utility functions for unittest2pytest.
 #
 
 __author__ = "Hartmut Goebel <h.goebel@crazy-compilers.com>"
-__copyright__ = "Copyright 2015-1017 by Hartmut Goebel"
+__copyright__ = "Copyright 2015-2019 by Hartmut Goebel"
 __licence__ = "GNU General Public License version 3 or later (GPLv3+)"
 
 
