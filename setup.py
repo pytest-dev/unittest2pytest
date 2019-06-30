2c32cd693230d1b04de1d546d362d49fa891e7aa
Hartmut Goebel <h.goebel@crazy-compilers.com>
2019-06-30 16:28:27 +0200

    Drop support for Python 3.4.
    Python 3.4 is end of life. It is no longer receiving bug fixes,
    including for security issues. It went EOL on 2019-03-18.
    
    Removing support for EOL Pythons will reduce testing resources.

--- setup.py
+++ setup.py
@@ -72,13 +72,12 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development",
         "Topic :: Utilities",
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     zip_safe=False
 )
