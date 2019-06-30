9c50a7f374d0bfaad72076bac633699d8ca5e40c
Hartmut Goebel <h.goebel@crazy-compilers.com>
2019-06-30 12:20:41 +0200

    Tests: Add missing test-cases for assertRaisesRegex.
    These should be kept in sync with assertRaisesRegexp.

--- tests/fixtures/self_assert/assertRaisesRegex_out.py
+++ tests/fixtures/self_assert/assertRaisesRegex_out.py
@@ -39,3 +39,11 @@ class TestRaises(TestCase):
             error(1, 2)
         with pytest.raises(RunTimeError):
             error(1, 2) or error()
+
+    def test_atom(self):
+        with pytest.raises(RunTimeError, match=("foo" "bar")):
+            someFunc(1,2,3)
+
+    def test_expr(self):
+        with pytest.raises(RunTimeError, match="foo" + "bar"):
+            someFunc(1,2,3)
