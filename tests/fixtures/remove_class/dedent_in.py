0a418c829b999d98001321fd45f7099818a03340
Oliver Bristow <evilumbrella+github@gmail.com>
2020-11-28 23:36:52 +0000

    Fix remove_class when methods have decorators

---
+++ tests/fixtures/remove_class/dedent_in.py
@@ -0,0 +1,13 @@
+class TestAssertNotEqual(TestCase):
+
+    def test_you(self):
+        pass
+
+
+    @property
+    def test_me(self):
+        [
+            item
+            for item in sequence
+            if item
+        ]
